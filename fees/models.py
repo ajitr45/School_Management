b3fb1fe03ff06dcbbfb70149e366bde44628f52b
Ajit Kumar <ajit62768@gmail.com>
2026-07-20 23:42:22 +0530

    student_fee

--- fees/models.py
+++ fees/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from academics.models import SchoolClass
+from students.models import Student
 
 # Create your models here.
 
@@ -28,3 +29,27 @@ class FeeStructure(models.Model):
 
     def __str__(self):
         return f"{self.school_class.name} ({self.academic_year}) - ₹{self.amount}"
+    
+    
+    
+    
+    
+class StudentFee(models.Model):
+
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("PAID", "Paid"),
+    ]
+
+    student = models.ForeignKey( Student, on_delete=models.CASCADE, related_name="fees",)
+    fee_structure = models.ForeignKey( FeeStructure, on_delete=models.CASCADE, related_name="student_fees")
+    amount_paid = models.DecimalField( max_digits=10, decimal_places=2, default=0)
+    payment_date = models.DateField( null=True, blank=True)
+    status = models.CharField( max_length=10, choices=STATUS_CHOICES, default="PENDING",)
+    receipt_number = models.CharField( max_length=50, unique=True,)
+    remarks = models.TextField( blank=True, null=True)
+    created_at = models.DateTimeField( auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.student.full_name} - {self.status}"
