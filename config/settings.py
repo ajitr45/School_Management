df64f0f3e155f0211beb9b86ecd4053cc2ca500c
Ajit Kumar <ajit62768@gmail.com>
2026-07-27 19:12:21 +0530

    Homework commit

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'fees',
     'exams',
     'timetable',
+    'homework',
     
     
     
---
+++ homework/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Homework
+
+
+class HomeworkSerializer(serializers.ModelSerializer):
+    
+    class_name = serializers.CharField(source = "school_class.name", read_only =True,)
+    section_name = serializers.CharField(source ="section.name",read_only =True)
+    subject_name = serializers.CharField(source = "subject.name", read_only = True)
+    teacher_name = serializers.CharField(source = 'teacher.full_name', read_only =True)
+    
+    
+    class Meta: 
+        
+        model = Homework
+        fields ="__all__"
+        
+        read_only = (
+            "created_at",
+            "updated_at",
+        )
+        
