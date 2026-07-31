a351a3b2088401903bb6634b551d86acda65da5e
Ajit Kumar <ajit62768@gmail.com>
2026-07-31 21:56:03 +0530

    jwt_auth commit

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,23 +1,31 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'role']
-        
-        
+        fields = ["id", "username", "email", "password", "role"]
+
         extra_kwargs = {
-            'password': {'write_only': True}
+            "password": {"write_only": True}
         }
-        
-        
-        def create(self, validated_data):
-            user = User(
-                username=validated_data['username'],
-                email=validated_data['email'],
-                role=validated_data['role']
-            )
-            user.set_password(validated_data['password'])
-            user.save()
-            return user 
+
+    def create(self, validated_data):
+
+        user = User(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            role=validated_data["role"]
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True)
