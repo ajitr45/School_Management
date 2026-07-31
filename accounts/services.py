8174c687f3fd9676503ab60526debe09291b7a1d
Ajit Kumar <ajit62768@gmail.com>
2026-07-31 23:21:01 +0530

    jwt commit

---
+++ accounts/services.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken    
+
+
+def login_user(validated_data):
+
+    username = validated_data["username"]
+    password = validated_data["password"]
+
+    user = authenticate( username=username, password=password,)
+
+    if user is None:
+        raise AuthenticationFailed(
+            "Invalid username or password."
+        )
+
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "role": user.role,
+        },
+    }
