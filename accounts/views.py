5f2a47068c1821a71633f4c17995834ea00f92bb
Ajit Kumar <ajit62768@gmail.com>
2026-08-02 23:56:30 +0530

    auth commit

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,28 @@ from .serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    
+    
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import LoginSerializer
+from .services import login_user
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+
+        serializer = LoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        data = login_user(serializer.validated_data)
+
+        return Response(
+            data,
+            status=status.HTTP_200_OK
+        )
