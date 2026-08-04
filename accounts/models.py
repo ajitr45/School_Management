61ef0c7f679c86c2374362e0be8c8d55800a9f02
Ajit Kumar <ajit62768@gmail.com>
2026-08-04 23:55:11 +0530

    resetPassword commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from datetime import timedelta
 
 
 # Create your models here.
@@ -17,3 +19,17 @@ class User(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=Role_CHOICES)
 
+
+def otp_expire():
+    return timezone.now() + timedelta(minutes=5)
+
+class PasswordResetOTP(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "password_reset_otps")
+    otp = models.CharField(max_length=6)
+    is_varified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(default=otp_expire)
+    
+    def __str__(self):
+        return f"OTP for {self.user.username} - {self.otp}"
