47a9126d6f917f9c49e88978c0e916878ec8d81a
Ajit Kumar <ajit62768@gmail.com>
2026-08-06 14:29:45 +0530

    complete user account module

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ForgotPasswordAPIView, UserViewSet, LoginAPIView,LogoutAPIView, ChangePasswordAPIView, VerifyOTPAPIView
+from .views import ForgotPasswordAPIView, ProfileAPIView, ResetPasswordAPIView, UserViewSet, LoginAPIView,LogoutAPIView, ChangePasswordAPIView, VerifyOTPAPIView
 
 
 router = DefaultRouter()
@@ -16,4 +16,7 @@ urlpatterns = [
     path("change-password/", ChangePasswordAPIView.as_view(), name="change-password"),
     path("forgot-password/", ForgotPasswordAPIView.as_view(), name="forgot-password"),
     path("verify-otp/", VerifyOTPAPIView.as_view(), name="verify-otp"),
+    path("reset-password/", ResetPasswordAPIView.as_view(), name="reset-password"),
+    path("profile/", ProfileAPIView.as_view(), name="profile"),
+    
 ]
