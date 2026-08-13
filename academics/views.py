f80d80021ff811275834e64b9ed4b717c204e679
Ajit Kumar <ajit62768@gmail.com>
2026-08-13 23:24:02 +0530

    Complete academics permission

--- academics/views.py
+++ academics/views.py
@@ -1,19 +1,105 @@
-
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from accounts.permissions import IsAdmin, IsAdminTeacherOrStudent
 from .models import SchoolClass, Section, Subject
-from .serializers import SchoolClassSerializer, SectionSerializer, SubjectSerializer
-
-# Create your views here.
+from .serializers import (SchoolClassSerializer, SectionSerializer, SubjectSerializer)
 
 
 class SchoolClassViewSet(ModelViewSet):
+
     queryset = SchoolClass.objects.all()
     serializer_class = SchoolClassSerializer
 
+    def get_permissions(self):
+
+        # Admin, Teacher and Student can view school classes.
+        if self.action in ["list", "retrieve"]:
+
+            permission_classes = [IsAdminTeacherOrStudent]
+
+        # Only Admin can create/update school classes.
+        elif self.action in ["create", "update", "partial_update"]:
+
+            permission_classes = [IsAdmin]
+
+        # DELETE is also handled here but destroy() below will block it.
+        else:
+
+            permission_classes = [IsAdmin]
+
+        return [permission() for permission in permission_classes]
+
+    def destroy(self,request):
+
+        return Response(
+            {
+                "detail": ("Deleting a school class is not allowed.")
+            },
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+
 class SectionViewSet(ModelViewSet):
+
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
+    def get_permissions(self):
+
+        # Admin, Teacher and Student can view sections.
+        if self.action in ["list", "retrieve"]:
+
+            permission_classes = [IsAdminTeacherOrStudent]
+
+        # Only Admin can create/update sections.
+
+        elif self.action in ["create", "update", "partial_update"]:
+
+            permission_classes = [IsAdmin]
+        else:
+            permission_classes = [IsAdmin]
+
+        return [permission() for permission in permission_classes]
+
+    def destroy(self, request):
+
+        return Response(
+            {
+                "detail": ("Deleting a section is not allowed.")
+            },
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+
 class SubjectViewSet(ModelViewSet):
+
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+    def get_permissions(self):
+
+        # Admin, Teacher and Student can view subjects.
+    
+        if self.action in ["list", "retrieve"]:
+
+            permission_classes = [IsAdminTeacherOrStudent]
+
+        # Only Admin can create/update subjects.
+        elif self.action in ["create", "update", "partial_update"]:
+
+            permission_classes = [IsAdmin]
+
+        else:
+            permission_classes = [IsAdmin]
+
+        return [permission() for permission in permission_classes]
+
+    def destroy( self, request):
+
+        return Response(
+            {
+                "detail": ("Deleting a subject is not allowed.")
+            },
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
