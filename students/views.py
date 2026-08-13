589f544e1c99b7529e4fdb8a9e77a9904d23d638
Ajit Kumar <ajit62768@gmail.com>
2026-08-13 22:58:45 +0530

    Complete student permission

--- students/views.py
+++ students/views.py
@@ -1,9 +1,66 @@
 from rest_framework import mixins, viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.models import User
+from accounts.permissions import IsAdmin, IsAdminTeacherOrStudent
 from .models import Student
 from .serializers import StudentSerializer
 
 
-class StudentViewSet( mixins.ListModelMixin, mixins.UpdateModelMixin,viewsets.GenericViewSet,mixins.RetrieveModelMixin,):
+class StudentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
 
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    def get_permissions(self):
+
+        # Admin, Teacher and Student can view student information.
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [IsAdminTeacherOrStudent]
+
+        # Only Admin can update student information.
+        # 
+        elif self.action in ["update", "partial_update"]:
+            permission_classes = [IsAdmin]
+
+        else:
+            permission_classes = [IsAdmin]
+
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+
+        # Admin and Teacher can access all student records.
+        return Student.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+
+        student = self.get_object()
+
+        # Student can access only his/her own record.
+    
+        if (request.user.role == User.STUDENT and student.user != request.user):
+            return Response(
+                {
+                    "detail": ("You do not have permission to access this student's information.")
+                },status=status.HTTP_403_FORBIDDEN,
+            )
+        serializer = self.get_serializer(student)
+
+        return Response( serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request):
+
+        # Student should see only his/her own record.
+        if request.user.role == User.STUDENT:
+
+            students = Student.objects.filter(user=request.user)
+            serializer = self.get_serializer(students, many=True)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Admin and Teacher can see all student records.
+        students = Student.objects.all()
+        serializer = self.get_serializer(students, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
