e8e227a921c3a9f7e891e62a5afd7a5fb2ec7330
Ajit Kumar <ajit62768@gmail.com>
2026-08-21 21:31:17 +0530

    Fronted and backend merge

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>School Management System</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({baseURL: "http://127.0.0.1:8000/api/"});
+
+api.interceptors.request.use((config) => {
+    const accessToken = localStorage.getItem("access");
+
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+});
+
+export default api;
