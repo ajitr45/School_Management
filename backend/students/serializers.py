00e462047bc42138ea9b1d4a423e8d4a0537fec7
Ajit Kumar <ajit62768@gmail.com>
2026-08-24 18:43:24 +0530

    enhance student serializer

--- backend/students/serializers.py
+++ backend/students/serializers.py
@@ -1,12 +1,49 @@
 from rest_framework import serializers
-
 from .models import Student
+from accounts.models import User
+from academics.models import SchoolClass, Section
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        ]
+
+
+class SchoolClassSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SchoolClass
+        fields = [
+            "id",
+            "name",
+        ]
+
+
+class SectionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Section
+        fields = [
+            "id",
+            "name",
+        ]
 
 
 class StudentSerializer(serializers.ModelSerializer):
 
-    class Meta:
+    user = UserSerializer(read_only=True)
+    school_class = SchoolClassSerializer(read_only=True)
+    section = SectionSerializer(read_only=True)
 
+    class Meta:
         model = Student
 
         fields = "__all__"
