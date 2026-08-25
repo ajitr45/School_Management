299ae170b57b52bffe8c7f7090631b4cc7f66f75
Ajit Kumar <ajit62768@gmail.com>
2026-08-25 23:25:24 +0530

    Frontend App work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,19 @@ import AdminDashboard from "./pages/AdminDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Students from "./pages/Students";
 import StudentDetail from "./pages/StudentDetail";
-import EditStudent from "./pages/EditStudent";
+import StudentEdit from "./pages/StudentEdit";
 import Admissions from "./pages/Admissions";
 import AdmissionDetail from "./pages/AdmissionDetail";
+import Classes from "./pages/Classes";
+import Sections from "./pages/Sections";
+import Subjects from "./pages/Subjects";
+import AdmissionForm from "./pages/AdmissionForm";
+import TeacherCreate from "./pages/teachers/TeacherCreate";
+import TeacherDetail from "./pages/teachers/TeacherDetail";
+import TeacherEdit from "./pages/teachers/TeacherEdit";
+import Teachers from "./pages/teachers/Teachers";
+import TeacherAssignment from "./pages/teachers/TeacherAssignment";
+import TeacherAssignments from "./pages/teachers/TeacherAssignments";
 
 
 
@@ -18,53 +28,51 @@ function App() {
             <Routes>
 
                 {/* Login */}
-                <Route
-                    path="/"
-                    element={<Login />}
-                />
 
+                <Route path="/" element={<Login />}/>
 
                 {/* Admin Routes */}
+
                 <Route
                     path="/admin"
-                    element={
-                        <ProtectedRoute allowedRole="ADMIN">
-                            <AdminLayout />
-                        </ProtectedRoute>
-                    }
-                >
+                    element={<ProtectedRoute allowedRole="ADMIN"> <AdminLayout /> </ProtectedRoute>}>
 
                     {/* /admin */}
-                    <Route
-                        index
-                        element={<AdminDashboard />}
-                    />
+                    <Route index element={<AdminDashboard />}/>
 
                     {/* /admin/students */}
-                    <Route
-                        path="students"
-                        element={<Students />}
-                    />
 
-                    <Route 
-                        path="students/:id"
-                        element={<StudentDetail/>}
-                    />
+                    <Route path="students" element={<Students />}/>
 
-                    <Route
-                        path="students/:id/edit"
-                        element={<EditStudent/>}
-                    />
+                    <Route path="students/:id" element={<StudentDetail/>}/>
 
-                    <Route  
-                        path="admissions"
-                        element={<Admissions/>}
-                    />
+                    <Route path="students/:id/edit" element={<StudentEdit/>}/>
 
-                    <Route 
-                        path="admissions/:id"
-                        element={<AdmissionDetail/>}
-                    />
+                    <Route  path="admissions" element={<Admissions/>}/>
+
+                    <Route path="admissions/:id" element={<AdmissionDetail/>}/>
+
+                    <Route path="Classes" element={<Classes />}/>
+
+                    <Route path="sections" element={<Sections />} />
+
+                    <Route path="subjects" element={<Subjects />} />
+
+                    <Route path="admissions/apply/" element={<AdmissionForm />} />
+
+                    <Route path="teachers" element={<Teachers/>} />
+
+                    <Route path="teachers/create/" element={<TeacherCreate />} />
+
+                    <Route path="teachers/:id" element={<TeacherDetail />} />
+
+                    <Route path="teachers/:id/edit" element={<TeacherEdit />} />
+
+                    <Route path="teachers/:id/assign" element={<TeacherAssignment />} />
+
+                    <Route path="teachers/assignments" element={<TeacherAssignments />} />
+
+                    
 
                 </Route>
 
