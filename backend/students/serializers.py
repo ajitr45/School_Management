6cd46801c9a81f636b637f85707ff0337c952547
Ajit Kumar <ajit62768@gmail.com>
2026-08-25 23:23:06 +0530

    serializer works

--- backend/students/serializers.py
+++ backend/students/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+
 from .models import Student
 from accounts.models import User
 from academics.models import SchoolClass, Section
+from admissions.models import Admission
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -37,19 +39,96 @@ class SectionSerializer(serializers.ModelSerializer):
         ]
 
 
+class StudentAdmissionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Admission
+        fields = "__all__"
+
+
 class StudentSerializer(serializers.ModelSerializer):
 
-    user = UserSerializer(read_only=True)
-    school_class = SchoolClassSerializer(read_only=True)
-    section = SectionSerializer(read_only=True)
+    # User sirf display ke liye
+    user = UserSerializer(
+        read_only=True
+    )
+
+    # Admission editable hai
+    admission = StudentAdmissionSerializer()
+
+    # Class ka naam display ke liye
+    school_class_detail = SchoolClassSerializer(
+        source="school_class",
+        read_only=True
+    )
+
+    # Section ka naam display ke liye
+    section_detail = SectionSerializer(
+        source="section",
+        read_only=True
+    )
+
+    # Class editable
+    school_class = serializers.PrimaryKeyRelatedField(
+        queryset=SchoolClass.objects.all()
+    )
+
+    # Section editable
+    section = serializers.PrimaryKeyRelatedField(
+        queryset=Section.objects.all()
+    )
 
     class Meta:
         model = Student
 
-        fields = "__all__"
+        fields = [
+            "id",
+            "user",
+            "admission",
+            "student_id",
+            "roll_number",
+            "school_class",
+            "school_class_detail",
+            "section",
+            "section_detail",
+            "admission_date",
+        ]
 
         read_only_fields = [
+            "id",
+            "user",
             "student_id",
             "roll_number",
             "admission_date",
         ]
+
+    def update(self, instance, validated_data):
+
+        # Admission data nikal lo
+        admission_data = validated_data.pop(
+            "admission",
+            None
+        )
+
+        # Admission update
+        if admission_data:
+
+            admission = instance.admission
+
+            for field, value in admission_data.items():
+
+                setattr(
+                    admission,
+                    field,
+                    value
+                )
+
+            admission.save()
+
+        # Student update
+        instance = super().update(
+            instance,
+            validated_data
+        )
+
+        return instance
