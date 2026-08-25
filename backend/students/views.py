f9f23a0eb86f07d40afa2fc178081f0e58d33fcb
Ajit Kumar <ajit62768@gmail.com>
2026-08-25 23:23:52 +0530

    Views works

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,40 +1,50 @@
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 from rest_framework import status
+
 from accounts.models import User
 from accounts.permissions import IsAdmin, IsAdminTeacherOrStudent
+
 from .models import Student
 from .serializers import StudentSerializer
 
 
-class StudentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+class StudentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet
+):
 
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
     def get_permissions(self):
 
-        # Admin, Teacher and Student can view student information.
         if self.action in ["list", "retrieve"]:
             permission_classes = [IsAdminTeacherOrStudent]
 
-        # Only Admin can update student information.
-        # 
         elif self.action in ["update", "partial_update"]:
             permission_classes = [IsAdmin]
 
         else:
             permission_classes = [IsAdmin]
 
-        return [permission() for permission in permission_classes]
+        return [
+            permission()
+            for permission in permission_classes
+        ]
 
     def get_queryset(self):
-        
+
+        # Student sirf apna record dekh sakta hai
         if self.request.user.role == User.STUDENT:
-            
-            return Student.objects.filter(user = self.request.user)
-        
-        # Admin and Teacher can access all student records.
+
+            return Student.objects.filter(
+                user=self.request.user
+            )
+
+        # Admin aur Teacher sabhi students dekh sakte hain
         return Student.objects.all()
 
     def retrieve(self, request, *args, **kwargs):
@@ -43,11 +53,21 @@ class StudentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.Up
 
         serializer = self.get_serializer(student)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
 
     def list(self, request, *args, **kwargs):
 
         students = self.get_queryset()
-        serializer = self.get_serializer(students, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(
+            students,
+            many=True
+        )
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
