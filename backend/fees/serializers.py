4bdca276783b0a16494396828818be8732e3ec82
Ajit Kumar <ajit62768@gmail.com>
2026-08-27 23:12:19 +0530

    Changes on fields

--- backend/fees/serializers.py
+++ backend/fees/serializers.py
@@ -12,19 +12,33 @@ class FeeStructureSerializer(serializers.ModelSerializer):
 
 class StudentFeeSerializer(serializers.ModelSerializer):
 
-    student_name = serializers.CharField(source="student.admission.student_name", read_only=True,)
+    student_name = serializers.CharField(source="student.admission.student_name", read_only=True)
+    student_id = serializers.CharField(source="student.student_id", read_only=True)
 
     class Meta:
         model = StudentFee
-        fields = "__all__"
-        
-        read_only_fields = (
+
+        fields = [
+            "id",
+            "student",
+            "student_id",
+            "student_name",
+            "fee_structure",
             "status",
             "assigned_date",
             "created_at",
             "updated_at",
-        )
+        ]
 
+        read_only_fields = [
+            "id",
+            "student_id",
+            "student_name",
+            "status",
+            "assigned_date",
+            "created_at",
+            "updated_at",
+        ]
 
 class FeePaymentSerializer(serializers.ModelSerializer):
 
