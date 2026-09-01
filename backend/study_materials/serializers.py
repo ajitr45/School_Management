85e963e8469e8696272f96c91dd4ab8813cc2f3c
Ajit Kumar <ajit62768@gmail.com>
2026-09-01 23:04:39 +0530

    serializer works

--- backend/study_materials/serializers.py
+++ backend/study_materials/serializers.py
@@ -1,22 +1,70 @@
-from .models import Chapter, StudyMaterial
 from rest_framework import serializers
+from .models import Chapter, StudyMaterial
 
 
 class ChapterSerializer(serializers.ModelSerializer):
-    
-    
+
     class Meta:
-        model= Chapter
+        model = Chapter
         fields = "__all__"
         read_only_fields = ["created_at"]
-        
-        
+
+
 class StudyMaterialSerializer(serializers.ModelSerializer):
-    
+
+    chapter_name = serializers.CharField(
+        source="chapter.name",
+        read_only=True,
+    )
+
+    chapter_number = serializers.IntegerField(
+        source="chapter.chapter_number",
+        read_only=True,
+    )
+
+    school_class = serializers.IntegerField(
+        source="chapter.school_class.id",
+        read_only=True,
+    )
+
+    subject = serializers.IntegerField(
+        source="chapter.subject.id",
+        read_only=True,
+    )
+
+    class_name = serializers.CharField(
+        source="chapter.school_class.name",
+        read_only=True,
+    )
+
+    subject_name = serializers.CharField(
+        source="chapter.subject.name",
+        read_only=True,
+    )
+
     class Meta:
-        
         model = StudyMaterial
-        fields = "__all__"
-        read_only_fields = ["created_at"]
-    
-    
+
+        fields = [
+            "id",
+            "chapter",
+            "chapter_name",
+            "chapter_number",
+            "school_class",
+            "class_name",
+            "subject",
+            "subject_name",
+            "description",
+            "file",
+            "created_at",
+        ]
+
+        read_only_fields = [
+            "created_at",
+            "chapter_name",
+            "chapter_number",
+            "school_class",
+            "class_name",
+            "subject",
+            "subject_name",
+        ]
