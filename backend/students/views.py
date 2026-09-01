f0346827fd97f987782cc29dc2c401f1aee71b07
Ajit Kumar <ajit62768@gmail.com>
2026-09-01 23:04:23 +0530

    views works

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,20 +1,14 @@
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 from rest_framework import status
-
 from accounts.models import User
 from accounts.permissions import IsAdmin, IsAdminTeacherOrStudent
-
 from .models import Student
 from .serializers import StudentSerializer
+from teachers.models import TeacherAssignment
 
 
-class StudentViewSet(
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet
-):
+class StudentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
 
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
@@ -35,39 +29,38 @@ class StudentViewSet(
             for permission in permission_classes
         ]
 
-    def get_queryset(self):
+def get_queryset(self):
 
-        # Student sirf apna record dekh sakta hai
-        if self.request.user.role == User.STUDENT:
+    
+    if self.request.user.role == User.STUDENT:
 
-            return Student.objects.filter(
-                user=self.request.user
-            )
-
-        # Admin aur Teacher sabhi students dekh sakte hain
-        return Student.objects.all()
-
-    def retrieve(self, request, *args, **kwargs):
-
-        student = self.get_object()
-
-        serializer = self.get_serializer(student)
-
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
+        return Student.objects.filter(
+            user=self.request.user
         )
 
-    def list(self, request, *args, **kwargs):
+    # TEACHER
+    if self.request.user.role == User.TEACHER:
 
-        students = self.get_queryset()
+        assignments = TeacherAssignment.objects.filter(teacher=self.request.user.teacher)
+        students = Student.objects.none()
 
-        serializer = self.get_serializer(
-            students,
-            many=True
-        )
+        for assignment in assignments:
+            students |= Student.objects.filter( school_class=assignment.school_class, section=assignment.section)
 
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
+        return students.distinct()
+
+    return Student.objects.all()
+
+def retrieve(self, request, *args, **kwargs):
+
+    student = self.get_object()
+    serializer = self.get_serializer(student)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+def list(self, request, *args, **kwargs):
+
+    students = self.get_queryset()
+    serializer = self.get_serializer(students, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
