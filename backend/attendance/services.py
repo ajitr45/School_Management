0ecb0971fd1db158b71a04a12873db585bdfcde4
Ajit Kumar <ajit62768@gmail.com>
2026-09-01 23:02:18 +0530

    Services work

--- backend/attendance/services.py
+++ backend/attendance/services.py
@@ -1,24 +1,22 @@
 from rest_framework.exceptions import ValidationError
 from .models import Attendance
+from teachers.models import TeacherAssignment
 
 
 def create_attendance(request, validated_data):
 
     teacher = request.user.teacher
+    student = validated_data["student"]
+    is_assigned = TeacherAssignment.objects.filter( teacher=teacher, school_class=student.school_class, section=student.section).exists()
 
-    attendance_exists = Attendance.objects.filter(
-        student=validated_data["student"],
-        date=validated_data["date"],).exists()
+    if not is_assigned:
+        raise ValidationError({"student": "You cannot mark attendance for this student."})
+
+    attendance_exists = Attendance.objects.filter( student=student, date=validated_data["date"]).exists()
 
     if attendance_exists:
-        raise ValidationError({
-            "detail": "Attendance already marked for this student on this date."
-        })
-
-    attendance = Attendance.objects.create(
-        teacher=teacher,
-        **validated_data,
-    )
+        raise ValidationError({"detail": "Attendance already marked for this student on this date."})
+    attendance = Attendance.objects.create(teacher=teacher, **validated_data,)
 
     return attendance
 
